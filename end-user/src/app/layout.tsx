16ff677da1a4c596661389577c7d1e68704b0a80
Garda Arraniri <garda.arraniri@gmail.com>
2024-02-06 15:25:25 +0700

    `chore:` setup basic style

--- end-user/src/app/layout.tsx
+++ end-user/src/app/layout.tsx
@@ -1,9 +1,13 @@
+import './globals.css'
 import { JSX, ReactNode } from 'react'
+import { Providers } from './providers'
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>): JSX.Element {
     return (
         <html lang='en'>
-            <body>{children}</body>
+            <body>
+                <Providers>{children}</Providers>
+            </body>
         </html>
     )
 }
--- end-user/src/app/page.tsx
+++ end-user/src/app/page.tsx
@@ -1,5 +1,9 @@
 import { JSX } from 'react'
 
 export default function Home(): JSX.Element {
-    return <h1>Hello World!</h1>
+    return (
+        <div className='flex h-screen items-center justify-center'>
+            <h1 className=''>Hello World!</h1>
+        </div>
+    )
 }
