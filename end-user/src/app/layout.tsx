e8dba6deb3b597d894d0e34f276b74ad80c1431b
Garda Arraniri <garda.arraniri@gmail.com>
2024-02-16 09:55:43 +0700

    `chore:` add metadata

--- end-user/src/app/layout.tsx
+++ end-user/src/app/layout.tsx
@@ -4,6 +4,26 @@ import { Providers } from './providers'
 import LandingPageNavbar from '@/components/Navbar/LandingPageNavbar'
 import LandingPageFooter from '@/components/Footer/LandingPageFooter'
 import ScrollToTop from '@/components/ScrollToTop'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    metadataBase: new URL('https://healing-oasis.vercel.app'), // later change this url with real domain
+    title: 'Healing Oasis | Massage and Spa Singapore',
+    description:
+        'Revitalize your mind, body, and spirit with spa & massage treatments at Healing Oasis. Book your session now and feel the comfort.',
+    keywords: [
+        'massage in tanjong pagar',
+        'tanjong pagar massage',
+        'massage near tanjong pagar',
+        'best massage in singapore',
+    ],
+    alternates: {
+        canonical: '/',
+    },
+    openGraph: {
+        images: '/webp/asset-og-healing-oasis.webp',
+    },
+}
 
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>): JSX.Element {
     return (
